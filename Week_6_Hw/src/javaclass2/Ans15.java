2062b98e4909bf1974c1fdb5d8ecd1af39f04390
kalptaru22 <pmaulik804@gmail.com>
2024-04-07 23:26:05 +0100

    sunday work

---
+++ Week_6_Hw/src/javaclass2/Ans15.java
@@ -0,0 +1,19 @@
+package javaclass2;
+
+public class Ans15 {
+    /*
+    Write a Java program to convert a given string into lowercase.
+    Sample Input: THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+    Output: the quick brown fox jumps over the lazy dog.
+     */
+    static String a = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.";
+
+    public static void main(String[] args) {
+        upperToLowerCase();
+    }
+
+    public static void upperToLowerCase() {
+        System.out.println(a.toLowerCase());
+    }
+
+}
